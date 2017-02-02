114da6c21ae31ab7bab4859e28ade6bc19a8d89d
Robert Worgul <r.worgul@stud.fh-sm.de>
2017-02-02 15:25:51 +0100

    explicitly write private

--- ResponseProcessor.hpp
+++ ResponseProcessor.hpp
@@ -7,13 +7,14 @@
 #define __RESPONSEPROCESSOR_HPP 1
 
 class ResponseProcessor {
-  const int THRESHOLD_MIN = 200;
-  const int THRESHOLD_MAX = 500;
-  const int MIN_PRESSED_TIME = 10;
-  bool alreadyPressed = false;
-  std::vector<long> pressedTimes;
-  unsigned long l;
-  bool isPressed(int);
+  private:
+    const int THRESHOLD_MIN = 200;
+    const int THRESHOLD_MAX = 500;
+    const int MIN_PRESSED_TIME = 10;
+    bool alreadyPressed = false;
+    std::vector<long> pressedTimes;
+    unsigned long l;
+    bool isPressed(int);
   public:
     ResponseProcessor();
     void addMeasurement(int);
